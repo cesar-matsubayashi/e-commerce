234588b5ad6e614fa483978a468de280b19d8a8b
Cesar Matsubayashi <cesar.matsubayashi@gmail.com>
2025-03-21 18:54:20 -0300

    feat: add customer xml presenter

--- src/infrastructure/api/__tests__/customer.e2e.spec.ts
+++ src/infrastructure/api/__tests__/customer.e2e.spec.ts
@@ -39,6 +39,63 @@ describe("E2E test for customer", () => {
     expect(response.status).toBe(500);
   });
 
+  it("should find a customer", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "John",
+        address: {
+          street: "Street",
+          city: "City",
+          number: 123,
+          zip: "12345",
+        },
+      });
+
+    expect(response.status).toBe(200);
+
+    const customer = await request(app)
+      .get(`/customer/${response.body.id}`)
+      .send();
+
+    expect(customer.status).toBe(200);
+    expect(customer.body.name).toBe("John");
+    expect(customer.body.address.street).toBe("Street");
+  });
+
+  it("should find a customer xml", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "John",
+        address: {
+          street: "Street",
+          city: "City",
+          number: 123,
+          zip: "12345",
+        },
+      });
+
+    expect(response.status).toBe(200);
+
+    const customer = await request(app)
+      .get(`/customer/${response.body.id}`)
+      .set("Accept", "application/xml")
+      .send();
+
+    expect(customer.status).toBe(200);
+    expect(customer.text).toContain(`<?xml version="1.0" encoding="UTF-8"?>`);
+    expect(customer.text).toContain(`<customer>`);
+    expect(customer.text).toContain(`<name>John</name>`);
+    expect(customer.text).toContain(`<address>`);
+    expect(customer.text).toContain(`<street>Street</street>`);
+    expect(customer.text).toContain(`<city>City</city>`);
+    expect(customer.text).toContain(`<number>123</number>`);
+    expect(customer.text).toContain(`<zip>12345</zip>`);
+    expect(customer.text).toContain(`</address>`);
+    expect(customer.text).toContain(`</customer>`);
+  });
+
   it("should list all customers", async () => {
     const response = await request(app)
       .post("/customer")
@@ -80,5 +137,132 @@ describe("E2E test for customer", () => {
     const customer2 = listResponse.body.customers[1];
     expect(customer2.name).toBe("Jane");
     expect(customer2.address.street).toBe("Street 2");
+  });
+
+  it("should list all customers xml", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "John",
+        address: {
+          street: "Street",
+          city: "City",
+          number: 123,
+          zip: "12345",
+        },
+      });
+    expect(response.status).toBe(200);
+
+    const response2 = await request(app)
+      .post("/customer")
+      .send({
+        name: "Jane",
+        address: {
+          street: "Street 2",
+          city: "City 2",
+          number: 1234,
+          zip: "12345",
+        },
+      });
+    expect(response2.status).toBe(200);
+
+    const listResponseXML = await request(app)
+      .get("/customer")
+      .set("Accept", "application/xml")
+      .send();
+
+    expect(listResponseXML.status).toBe(200);
+    expect(listResponseXML.text).toContain(
+      `<?xml version="1.0" encoding="UTF-8"?>`
+    );
+    expect(listResponseXML.text).toContain(`<customers>`);
+    expect(listResponseXML.text).toContain(`<customer>`);
+    expect(listResponseXML.text).toContain(`<name>John</name>`);
+    expect(listResponseXML.text).toContain(`<address>`);
+    expect(listResponseXML.text).toContain(`<street>Street</street>`);
+    expect(listResponseXML.text).toContain(`<city>City</city>`);
+    expect(listResponseXML.text).toContain(`<number>123</number>`);
+    expect(listResponseXML.text).toContain(`<zip>12345</zip>`);
+    expect(listResponseXML.text).toContain(`</address>`);
+    expect(listResponseXML.text).toContain(`</customer>`);
+    expect(listResponseXML.text).toContain(`<name>Jane</name>`);
+    expect(listResponseXML.text).toContain(`<street>Street 2</street>`);
+    expect(listResponseXML.text).toContain(`<city>City 2</city>`);
+    expect(listResponseXML.text).toContain(`<number>1234</number>`);
+    expect(listResponseXML.text).toContain(`</customers>`);
+  });
+
+  it("should update a customer", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "John",
+        address: {
+          street: "Street",
+          city: "City",
+          number: 123,
+          zip: "12345",
+        },
+      });
+    expect(response.status).toBe(200);
+
+    const input = {
+      id: response.body.id,
+      name: "John Updated",
+      address: {
+        street: "Street Updated",
+        number: 1234,
+        zip: "12345678",
+        city: "City Updated",
+      },
+    };
+    const output = await request(app)
+      .put(`/customer/${response.body.id}`)
+      .send(input);
+
+    expect(output.status).toBe(200);
+    expect(output.body).toEqual(input);
+  });
+
+  it("should update a customer xml", async () => {
+    const response = await request(app)
+      .post("/customer")
+      .send({
+        name: "John",
+        address: {
+          street: "Street",
+          city: "City",
+          number: 123,
+          zip: "12345",
+        },
+      });
+    expect(response.status).toBe(200);
+
+    const input = {
+      id: response.body.id,
+      name: "John Updated",
+      address: {
+        street: "Street Updated",
+        number: 1234,
+        zip: "12345678",
+        city: "City Updated",
+      },
+    };
+    const output = await request(app)
+      .put(`/customer/${response.body.id}`)
+      .set("Accept", "application/xml")
+      .send(input);
+
+    expect(output.status).toBe(200);
+    expect(output.text).toContain(`<?xml version="1.0" encoding="UTF-8"?>`);
+    expect(output.text).toContain(`<customer>`);
+    expect(output.text).toContain(`<name>John Updated</name>`);
+    expect(output.text).toContain(`<address>`);
+    expect(output.text).toContain(`<street>Street Updated</street>`);
+    expect(output.text).toContain(`<city>City Updated</city>`);
+    expect(output.text).toContain(`<number>1234</number>`);
+    expect(output.text).toContain(`<zip>12345678</zip>`);
+    expect(output.text).toContain(`</address>`);
+    expect(output.text).toContain(`</customer>`);
   });
 });
--- src/infrastructure/api/routes/customer.route.ts
+++ src/infrastructure/api/routes/customer.route.ts
@@ -2,6 +2,9 @@ import express, { Request, Response } from "express";
 import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
 import CustomerRepository from "../../customer/repository/sequelize/customer.repository";
 import ListCustomerUseCase from "../../../usecase/customer/list/list.customer.usecase";
+import CustomerPresenter from "../presenters/customer.presenter";
+import FindCustomerUseCase from "../../../usecase/customer/find/find.customer.usecase";
+import UpdateCustomerUseCase from "../../../usecase/customer/update/update.customer.usecase";
 
 export const customerRoute = express.Router();
 
@@ -32,7 +35,50 @@ customerRoute.get("/", async (req: Request, res: Response) => {
 
   try {
     const output = await usecase.execute({});
-    res.send(output);
+    res.format({
+      json: async () => res.send(output),
+      xml: async () => res.send(CustomerPresenter.listXML(output)),
+    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+customerRoute.get("/:id", async (req: Request, res: Response) => {
+  const usecase = new FindCustomerUseCase(new CustomerRepository());
+
+  try {
+    const output = await usecase.execute({ id: req.params.id });
+    res.format({
+      json: async () => res.send(output),
+      xml: async () => res.send(CustomerPresenter.toXML(output)),
+    });
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+customerRoute.put("/:id", async (req: Request, res: Response) => {
+  const usecase = new UpdateCustomerUseCase(new CustomerRepository());
+
+  try {
+    const customerDto = {
+      id: req.params.id,
+      name: req.body.name,
+      address: {
+        street: req.body.address.street,
+        city: req.body.address.city,
+        number: req.body.address.number,
+        zip: req.body.address.zip,
+      },
+    };
+
+    const output = await usecase.execute(customerDto);
+
+    res.format({
+      json: async () => res.send(output),
+      xml: async () => res.send(CustomerPresenter.toXML(output)),
+    });
   } catch (err) {
     res.status(500).send(err);
   }
