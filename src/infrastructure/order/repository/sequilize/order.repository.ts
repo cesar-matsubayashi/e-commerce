279c9e004480d9c6732fa2cf7f27c12f6d5c7656
Cesar Matsubayashi <cesar.matsubayashi@gmail.com>
2025-03-23 19:12:31 -0300

    feat: add order repository update, find and findAll

--- src/infrastructure/order/repository/sequilize/order.repository.ts
+++ src/infrastructure/order/repository/sequilize/order.repository.ts
@@ -1,4 +1,5 @@
 import Order from "../../../../domain/checkout/entity/order";
+import OrderItem from "../../../../domain/checkout/entity/order_item";
 import OrderItemModel from "./order-item.model";
 import OrderModel from "./order.model";
 
@@ -21,5 +22,96 @@ export default class OrderRepository {
         include: [{ model: OrderItemModel }],
       }
     );
+  }
+
+  async update(entity: Order): Promise<void> {
+    await OrderModel.update(
+      {
+        customer_id: entity.customerId,
+        total: entity.total(),
+      },
+      {
+        where: {
+          id: entity.id,
+        },
+      }
+    );
+
+    await Promise.all(
+      entity.items.map(async (item) => {
+        OrderItemModel.update(
+          {
+            name: item.name,
+            price: item.price,
+            product_id: item.productId,
+            quantity: item.quantity,
+          },
+          {
+            where: {
+              id: item.id,
+            },
+          }
+        );
+      })
+    );
+  }
+
+  async find(id: string): Promise<Order> {
+    let orderModel;
+    try {
+      orderModel = await OrderModel.findOne({
+        where: {
+          id,
+        },
+        include: ["items"],
+        rejectOnEmpty: true,
+      });
+    } catch (error) {
+      throw new Error("Order not found");
+    }
+
+    const orderItem: OrderItem[] = [];
+
+    orderModel.items.forEach((item) => {
+      orderItem.push(
+        new OrderItem(
+          item.id,
+          item.name,
+          item.price,
+          item.product_id,
+          item.quantity
+        )
+      );
+    });
+
+    const order = new Order(orderModel.id, orderModel.customer_id, orderItem);
+    return order;
+  }
+
+  async findAll(): Promise<Order[]> {
+    const orderModels = await OrderModel.findAll({ include: ["items"] });
+    const orders = orderModels.map((orderModels) => {
+      let orderItems: OrderItem[] = [];
+      orderModels.items.forEach((item) => {
+        orderItems.push(
+          new OrderItem(
+            item.id,
+            item.name,
+            item.price,
+            item.product_id,
+            item.quantity
+          )
+        );
+      });
+
+      let order = new Order(
+        orderModels.id,
+        orderModels.customer_id,
+        orderItems
+      );
+      return order;
+    });
+
+    return orders;
   }
 }
