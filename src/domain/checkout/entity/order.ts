a84e94f53a1c0bc3c8b1e204f36a13b0278ad152
Cesar Matsubayashi <cesar.matsubayashi@gmail.com>
2025-03-23 23:45:21 -0300

    feat: add Order and OrderItem validator

--- src/domain/checkout/entity/order.ts
+++ src/domain/checkout/entity/order.ts
@@ -1,20 +1,24 @@
+import Entity from "../../@shared/entity/entity.abstract";
+import NotificationError from "../../@shared/notification/notification.error";
+import OrderValidatorFactory from "../factory/order.validator.factory";
 import OrderItem from "./order_item";
-export default class Order {
-  private _id: string;
+
+export default class Order extends Entity {
   private _customerId: string;
   private _items: OrderItem[];
   private _total: number;
 
   constructor(id: string, customerId: string, items: OrderItem[]) {
+    super();
     this._id = id;
     this._customerId = customerId;
     this._items = items;
     this._total = this.total();
     this.validate();
-  }
 
-  get id(): string {
-    return this._id;
+    if (this.notification.hasErrors()) {
+      throw new NotificationError(this.notification.getErrors());
+    }
   }
 
   get customerId(): string {
@@ -36,22 +40,8 @@ export default class Order {
     });
   }
 
-  validate(): boolean {
-    if (this._id.length === 0) {
-      throw new Error("Id is required");
-    }
-    if (this._customerId.length === 0) {
-      throw new Error("CustomerId is required");
-    }
-    if (this._items.length === 0) {
-      throw new Error("Items are required");
-    }
-
-    if (this._items.some((item) => item.quantity <= 0)) {
-      throw new Error("Quantity must be greater than 0");
-    }
-
-    return true;
+  validate() {
+    OrderValidatorFactory.create().validate(this);
   }
 
   total(): number {
---
+++ src/domain/checkout/validator/order.yup.validator.ts
@@ -0,0 +1,52 @@
+import ValidatorInterface from "../../@shared/validator/validator.interface";
+import * as yup from "yup";
+import Order from "../entity/order";
+
+export default class OrderYupValidator implements ValidatorInterface<Order> {
+  validate(entity: Order): void {
+    try {
+      yup
+        .object()
+        .shape({
+          id: yup.string().required("Id is required"),
+          customerId: yup.string().required("CustomerId is required"),
+          items: yup
+            .array()
+            .of(
+              yup.object({
+                id: yup.string().required("Item id is required"),
+                name: yup.string().required("Item name is required"),
+                productId: yup.string().required("Item productId is required"),
+                quantity: yup
+                  .number()
+                  .required("Item quantity is required")
+                  .min(1, "Quantity must be greater than 0"),
+                price: yup
+                  .number()
+                  .required("Item price is required")
+                  .min(0.01, "Item price must be greater than zero"),
+              })
+            )
+            .min(1, "Items are required"),
+        })
+        .validateSync(
+          {
+            id: entity.id,
+            customerId: entity.customerId,
+            items: entity.items,
+          },
+          {
+            abortEarly: false,
+          }
+        );
+    } catch (errors) {
+      const e = errors as yup.ValidationError;
+      e.errors.forEach((error) => {
+        entity.notification.addError({
+          context: "order",
+          message: error,
+        });
+      });
+    }
+  }
+}
