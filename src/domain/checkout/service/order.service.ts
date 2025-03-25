908aadd229e4270f5d1df511a5214b08ccf67471
Cesar Matsubayashi <cesar.matsubayashi@gmail.com>
2025-03-24 23:54:58 -0300

    feat: use OrderFactory in OrderService

--- src/domain/checkout/service/order.service.ts
+++ src/domain/checkout/service/order.service.ts
@@ -1,14 +1,14 @@
 import Order from "../entity/order";
 import OrderItem from "../entity/order_item";
-import { v4 as uuid } from "uuid";
 import Customer from "../../customer/entity/customer";
+import OrderFactory from "../factory/order.factory";
 
 export default class OrderService {
   static placeOrder(customer: Customer, items: OrderItem[]): Order {
     if (items.length === 0) {
       throw new Error("Order must have at least one item");
     }
-    const order = new Order(uuid(), customer.id, items);
+    const order = OrderFactory.create(customer.id, items);
     customer.addRewardPoints(order.total() / 2);
     return order;
   }
